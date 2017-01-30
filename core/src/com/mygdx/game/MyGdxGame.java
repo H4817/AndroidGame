2b2243df7e77d4a02144e2b98c6310b9b70feedc
JakimychevNikolaj <jakimychev.nikolaj@gmail.com>
2017-01-30 20:16:58 +0300

    load objects from map

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -4,13 +4,21 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.maps.objects.CircleMapObject;
+import com.badlogic.gdx.maps.objects.EllipseMapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Ellipse;
+import com.badlogic.gdx.math.Vector;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -19,7 +27,6 @@ import java.util.ArrayList;
 
 public class MyGdxGame extends ApplicationAdapter {
     private SpriteBatch batch;
-    //    private Texture backgroundTexture;
     private Player protagonist;
     private TouchPad touchPad;
     private ArrayList<EasyEnemy> enemies;
@@ -54,30 +61,47 @@ public class MyGdxGame extends ApplicationAdapter {
         UpdateEnemies();
     }
 
+    private void CreateObjects() {
+        for (EllipseMapObject ellipseMapObject : mapObjects.getByType(EllipseMapObject.class)) {
+            if (ellipseMapObject.getName().equals("easyEnemy")) {
+                enemies.add(new EasyEnemy(new Vector2((Float) ellipseMapObject.getProperties().get("x"),
+                        (Float) ellipseMapObject.getProperties().get("y"))));
+            } else if (ellipseMapObject.getName().equals("player")) {
+                if (protagonist == null) {
+                    protagonist = new Player(new Vector2((Float) ellipseMapObject.getProperties().get("x"),
+                            (Float) ellipseMapObject.getProperties().get("y")));
+                } else {
+                    protagonist.SetPosition(new Vector2((Float) ellipseMapObject.getProperties().get("x"),
+                            (Float) ellipseMapObject.getProperties().get("y")));
+                    System.out.println("not null");
+                }
+            }
+        }
+    }
+
     @Override
     public void create() {
         batch = new SpriteBatch();
-//        backgroundTexture = new Texture("images/background.jpg");
-        protagonist = new Player(new Vector2(1300, 944));
         touchPad = new TouchPad();
         touchPad.create();
         enemies = new ArrayList<EasyEnemy>();
-        enemies.add(new EasyEnemy(new Vector2(100, 400)));
         tiledMap = new TmxMapLoader().load("levels/Level_1.tmx");
         renderer = new OrthogonalTiledMapRenderer(tiledMap);
-        camera = new OrthographicCamera();
-        viewport = new ExtendViewport(1980, 1050, camera);
         mapObjects = tiledMap.getLayers().get("objects").getObjects();
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        CreateObjects();
+        camera.position.set(protagonist.GetSprite().getX(), protagonist.GetSprite().getY(), 0);
+        viewport = new ExtendViewport(1920, 1200, camera);
     }
 
     @Override
     public void render() {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        camera.position.set(protagonist.GetPosition().x + protagonist.GetSprite().getWidth() / 2, protagonist.GetPosition().y + protagonist.GetSprite().getHeight() / 2, 0);
-        camera.update();
         renderer.setView(camera);
         renderer.render();
+        camera.position.set(protagonist.GetSprite().getX(), protagonist.GetSprite().getY(), 0);
+        camera.update();
         batch.begin();
         Update();
         batch.end();
@@ -86,8 +110,8 @@ public class MyGdxGame extends ApplicationAdapter {
 
     @Override
     public void resize(int width, int height) {
-        viewport.update(width, height);
-//        camera.viewportHeight = height / 10;
-//        camera.viewportWidth = width / 10;
+        camera.viewportWidth = width;
+        camera.viewportHeight = height;
+        camera.position.set(width / 2f, height / 2f, 0);
     }
 }
